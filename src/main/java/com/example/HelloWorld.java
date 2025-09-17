3ae62b5da174eb3c200a38697093f861f67dc6b7
savankansagra <savan.kansagra@cybrilla.com>
2025-09-17 09:52:05 +0530

    hello world for running the springboot application

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+/**
+ * A simple Hello World application in Java
+ */
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to your first Java Maven project!");
+    }
+}
